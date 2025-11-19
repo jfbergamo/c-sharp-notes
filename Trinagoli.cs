fc66e02e2d7391ba7ce0f47a56abbad326fa81d7
J <jacopobergamasco06@gmail.com>
2025-11-19 14:09:07 +0100

    implementato esercizio triangoli

--- Trinagoli.cs
+++ Trinagoli.cs
@@ -4,9 +4,13 @@ public class Triangoli
     {
         bool success = true;
 
-        if (!TestValidTriangle(12,6,6, true)) success = false;
-        if (!TestValidTriangle(1,2,3, true)) success = false;
-        if (!TestValidTriangle(0,7,25, true)) success = false;
+        // TODO: aggiungere dei test
+
+        if (!TestValidTriangle(12,6,6, false)) success = false;
+        if (!TestValidTriangle(1,2,3, false)) success = false;
+        if (!TestValidTriangle(0,7,25, false)) success = false;
+        if (!TestValidTriangle(12,12,6, true)) success = false;
+        if (!TestValidTriangle(7,7,7, true)) success = false;
 
         Console.WriteLine("{0}utti i test sono stati conclusi con successo.", success ? "T" : "Non t");
 
@@ -16,7 +20,9 @@ public class Triangoli
     {
         float p = (a+b+c)/2.0f;
         float d = p*(p-a)*(p-b)*(p-c);
-        return d > 0;
+        // float A = Math.Sqrt(d); // se d < 0 è fuori dal dominio di sqrt
+        return d > 0; // > 0 e non >= 0 perché sqrt(0) = 0 e 
+                     // un triangolo non può avere area = 0
     }
 
     static bool TestValidTriangle(int a, int b, int c, bool res)
