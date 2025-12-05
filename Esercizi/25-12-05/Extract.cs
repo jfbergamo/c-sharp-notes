5b3cc5f8e0358a71382082134a23abab4fba6b11
J <jacopobergamasco06@gmail.com>
2025-12-05 16:12:44 +0100

    Aggiunto ExctractValuesX

--- Esercizi/25-12-05/Extract.cs
+++ Esercizi/25-12-05/Extract.cs
@@ -1,5 +1,3 @@
-using System.Data;
-
 namespace Esercizi
 {
     public class Extract
@@ -24,6 +22,25 @@ namespace Esercizi
                 ArrayToString(ExtractValues(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})),
                 ArrayToString(new int[] {2, 4, 6, 8, 10})
             );
+            u.Add(
+                ArrayToString(ExtractValuesX(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, x => x % 2 == 0)),
+                ArrayToString(new int[] {2, 4, 6, 8, 10})
+            );
+            u.Add(
+                ArrayToString(ExtractValues(new int[] {2, 4, 6, 8, 10}, 1)),
+                ArrayToString(new int[] {2, 4, 6, 8, 10})
+            );
+
+            Func<int, bool> filter = (x) => x == 7;
+
+            u.Add(
+                ArrayToString(ExtractValuesX(new int[] {2, 4, 6, 8, 10}, filter)),
+                ArrayToString(new int[] {})
+            );
+            u.Add(
+                ArrayToString(ExtractValuesX(new int[] {7, 2, 4, 7, 6, 8, 7, 10}, filter)),
+                ArrayToString(new int[] {7, 7, 7})
+            );
 
             if (u.TestAll()) Console.WriteLine("Test superati.");
         }
@@ -38,6 +55,21 @@ namespace Esercizi
             for (int i = 0; i < values.Length; i++)
             {
                 if (values[i] % n == 0) result[count++] = values[i];
+            }
+            
+            return result;
+        }
+        
+        static int[] ExtractValuesX(int[] values, Func<int, bool> filter)
+        {
+            int count = 0;
+            foreach (int value in values) if (filter(value)) count++;
+
+            int[] result = new int[count];
+            count = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (filter(values[i])) result[count++] = values[i];
             }
             
             return result;
